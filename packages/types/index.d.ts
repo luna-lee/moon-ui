6552f4d4522cdc6af12c20cb72023eac2aab5f8d
lip_mac <1724956493@qq.com>
2025-04-09 14:09:58 +0800

    chore: remove types directory from git tracking

--- packages/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { App } from "vue";
-import MButton from "./button/Button.vue";
-export declare const MoonUI: {
-    install(app: App): App<any>;
-};
-export { MButton };
-export default MoonUI;
